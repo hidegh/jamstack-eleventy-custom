8bb6298a0a2d32aa0b1fae2e29733c859785bda6
balazs hideghety <balazs.hideghety@gmail.com>
2021-10-11 09:10:02 +0200

    default post collection should be in date asc. order;

--- src/scripts/collections/posts.js
+++ src/scripts/collections/posts.js
@@ -8,7 +8,10 @@ module.exports = {
 
         // return callback for eleventy.addCollection
         return function(collectionApi) {
-            const posts = collectionApi.getFilteredByGlob(globFilter).reverse();
+            const posts = collectionApi
+                .getFilteredByGlob(globFilter)
+                .sort((a, b) => (a.score - b.score) * +1 /* +1 asc, -1 desc */);
+                
             return posts;
         };
 
